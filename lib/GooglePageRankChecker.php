71940337258994237728d5e78aa6618201755e10
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2011-10-12 10:52:12 +0300

    Changed url structure for getting Google Page Rank

--- lib/GooglePageRankChecker.php
+++ lib/GooglePageRankChecker.php
@@ -96,7 +96,7 @@ class GooglePageRankChecker {
     // If a connection can be established
     if($socket) {
       // Prep socket headers
-      $out = "GET /search?client=navclient-auto&ch=".$this->checkHash($this->createHash($page))."&features=Rank&q=info:".$page."&num=100&filter=0 HTTP/1.1\r\n";
+      $out = "GET /tbr?client=navclient-auto&ch=".$this->checkHash($this->createHash($page))."&features=Rank&q=info:".$page."&num=100&filter=0 HTTP/1.1\r\n";
       $out .= "Host: toolbarqueries.google.com\r\n";
       $out .= "User-Agent: Mozilla/4.0 (compatible; GoogleToolbar 2.0.114-big; Windows XP 5.1)\r\n";
       $out .= "Connection: Close\r\n\r\n";
